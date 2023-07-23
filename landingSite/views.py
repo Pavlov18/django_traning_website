2d75e76e786c0937907a9c2fbafc3a51849f79e4
pavlov <pavlov.web@mail.ru>
2023-07-23 23:45:27 +0400

    2023-07-23 23:45

--- landingSite/views.py
+++ landingSite/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 
 def first_page(request):
     a = "<h1>Hello world</h1>"
+    b = "hello world 2"
     text = "Новый текст"
     return render(request, "./index.html",{
         "a": a,
-        "text": text
+        "text": text,
+        "b": b,
     })

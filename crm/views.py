c4494e042a9e9d035836f537991e78ec6196c50c
pavlov <pavlov.web@mail.ru>
2023-07-26 18:24:41 +0400

    Формы в jango. GET и POST запросы. Добавление записей в БД

--- crm/views.py
+++ crm/views.py
@@ -4,3 +4,18 @@ from .models import Order
 def first_page(request):
     object_list = Order.objects.all()
     return render(request, './index.html', {'object_list': object_list})
+
+# Создаём функцию, которая выводит html страницу Спасибо
+def thanks_page(request):
+    # Получение данных из запроса
+    name = request.POST['name']
+    phone = request.POST['phone']
+
+    # Сохранене данных в базу данных. Создаем экземляр класса и методом save сохраняем запрос в БД
+    element = Order(order_name=name, order_phone=phone)
+    element.save()
+
+    return render(request, './thanks_page.html',{
+        'name':name,
+        'phone':phone
+        })

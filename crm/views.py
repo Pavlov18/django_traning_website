0f5ea0c23a3d829f7efff9cfe84dc7e031d4cdf2
pavlov <pavlov.web@mail.ru>
2023-08-04 13:08:43 +0400

    Добавление приложения PRICE и таблицы с услугами (старые и новые цены

--- crm/views.py
+++ crm/views.py
@@ -2,12 +2,26 @@ from django.shortcuts import render
 from .models import Order
 from .forms import OrderForm
 from cms.models import CmsSlider
+from price.models import PriceCard, PriceTable
 
 # Create your views here.
 def first_page(request):
     slider_list = CmsSlider.objects.all()
-    form = OrderForm()
-    return render(request, './index.html', {'slider_list': slider_list,})
+    # Из класса PriceCard берем элементы по id через конструкцию GET с параметром id1 , 2 и 3
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    # Из второго класса (pricetable) берем все элементы и передаём в рендер
+    price_table = PriceTable.objects.all()
+    # Так как количество передаваемых объектов уже не малое, запишем их все в переменную (словарь) и передадим потом её вместо словаря (для красоты)
+    dict_obj = {
+        'slider_list': slider_list,
+        'pc_1': pc_1,
+        'pc_2': pc_2,
+        'pc_3': pc_3,
+        'price_table': price_table,
+    }
+    return render(request, './index.html', dict_obj)
 
 # Создаём функцию, которая выводит html страницу Спасибо
 def thanks_page(request):
